5411caad457237caf112894fd8420890f8c66a99
tminoshy <dev.minhdt2@gmail.com>
2026-03-11 10:08:06 +0700

    fix bug in login : password in userdeatils set to blank

--- src/main/java/minhdo/swe/project/security/CustomUserDetails.java
+++ src/main/java/minhdo/swe/project/security/CustomUserDetails.java
@@ -22,7 +22,7 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public @Nullable String getPassword() {
-        return "";
+        return user.getPasswordHash();
     }
 
     @Override
